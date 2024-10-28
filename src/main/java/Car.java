bd0989c540b66e02d255c1258b35fc5ac672c010
HYH <jijinny26@gmail.com>
2024-10-28 09:24:28 -0500

    add Car.java file

---
+++ src/main/java/Car.java
@@ -0,0 +1,30 @@
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getProgress() {
+        return "-".repeat(position);
+    }
+}
